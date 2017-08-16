a3fc641147f64a41eb110b226e957a0ec62f9dc6
Garvin <Garvin@local>
2017-08-15 23:52:04 -0400

    typo!!

--- test/DatatablesParser.Tests/test-runner.sh
+++ test/DatatablesParser.Tests/test-runner.sh
@@ -6,7 +6,7 @@ dotnet build
 echo "Testing connections to test db servers"
 while !  nc -w 1 mssql 1433 && nc -w 1 mysql 3306  ; do 
     sleep 1; 
-    echo "Test db serers not ready. Trying again"
+    echo "Test db servers not ready. Trying again"
 done
 echo "Test DB Servers started"
 dotnet test
