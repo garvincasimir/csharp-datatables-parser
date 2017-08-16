a679f4accaa14a4681312c3fa87fda97a9ad6166
Garvin <Garvin@local>
2017-08-16 00:13:17 -0400

    compose should return exit code from test runner

--- test/DatatablesParser.Tests/test-runner.sh
+++ test/DatatablesParser.Tests/test-runner.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 echo "Restore and build while db servers are starting"
+
 dotnet restore
 dotnet build
 echo "Testing connections to test db servers"
-while !  nc -w 1 mssql 1433 && nc -w 1 mysql 3306  ; do 
+while ! ( nc -w 1 mssql 1433 &> /dev/null && nc -w 1 mysql 3306 &> /dev/null ) ; do 
     sleep 1; 
     echo "Test db servers not ready. Trying again"
 done
